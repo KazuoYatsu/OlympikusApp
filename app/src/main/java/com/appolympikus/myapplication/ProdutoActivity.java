e54492bb177daea6057549e7275222b12dc53599
Unknown <arthur.gomes_4718@hotmail.com>
2018-03-24 13:04:31 -0300

    Metodo get para receber dados do intent main para o intent produto.

--- app/src/main/java/com/appolympikus/myapplication/ProdutoActivity.java
+++ app/src/main/java/com/appolympikus/myapplication/ProdutoActivity.java
@@ -1,13 +1,30 @@
 package com.appolympikus.myapplication;
 
+import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ProdutoActivity extends AppCompatActivity {
+
+    private TextView tv_titulo_ref, tv_descricao;
+    private ImageView img_thumbnail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_produto);
+
+        Intent intent = getIntent();
+
+        //Receber data do Intent da pagina do Recycler.
+
+        String Titulo_REF = intent.getExtras().getString("titulo_ref");
+        String Descricao = intent.getExtras().getString("descricao_produto");
+        int image = intent.getExtras().getInt("thumbnail");
+
+
     }
 }
