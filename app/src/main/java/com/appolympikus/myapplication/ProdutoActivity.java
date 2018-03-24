51206b4dc7dac896f770231418756a8b54fb5c49
Unknown <arthur.gomes_4718@hotmail.com>
2018-03-24 13:19:21 -0300

    Metodo get e set para o Activity produto.
    Classe pode encontrar os elementos xml correspondentes para escrever os dados que foram recebidos da Main.

--- app/src/main/java/com/appolympikus/myapplication/ProdutoActivity.java
+++ app/src/main/java/com/appolympikus/myapplication/ProdutoActivity.java
@@ -17,13 +17,26 @@ public class ProdutoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_produto);
 
-        Intent intent = getIntent();
+        //Encontrar os elementos no xml.
+        tv_titulo_ref = (TextView) findViewById(R.id.exibir_REF_id);
+        tv_descricao = (TextView) findViewById(R.id.exibir_descricao_id);
+        img_thumbnail = (ImageView) findViewById(R.id.produto_thumbnail_id);
+
 
         //Receber data do Intent da pagina do Recycler.
-
+        Intent intent = getIntent();
         String Titulo_REF = intent.getExtras().getString("titulo_ref");
         String Descricao = intent.getExtras().getString("descricao_produto");
         int image = intent.getExtras().getInt("thumbnail");
+
+        //Setar os valores nos elemenots do XML.
+        tv_titulo_ref.setText(Titulo_REF);
+        tv_descricao.setText(Descricao);
+        img_thumbnail.setImageResource(image);
+
+
+
+
 
 
     }
