4505b5ae96232e0ff41eefcac1b048c6a9e2364b
Unknown <arthur.gomes_4718@hotmail.com>
2018-03-26 10:51:40 -0300

    Compartilhamento - Facebook/Instagram.
    Adicionando a lógica para acessar o share de ambas as api, através do Graph api do facebook.

--- app/src/main/java/com/appolympikus/myapplication/EditarPostActivity.java
+++ app/src/main/java/com/appolympikus/myapplication/EditarPostActivity.java
@@ -1,20 +1,27 @@
 package com.appolympikus.myapplication;
 
-import android.content.Context;
 import android.content.Intent;
-import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.content.Context;
+import android.widget.Toast;
+
+
+import com.facebook.FacebookSdk;
 
 public class EditarPostActivity extends AppCompatActivity {
 
     private ImageView imagem_post_rede_social;
+    private Button btn_compartilhar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editar_post);
+        FacebookSdk.sdkInitialize(getApplicationContext());
 
         //getActionBar().hide();
         getSupportActionBar().hide();
@@ -24,6 +31,19 @@ public class EditarPostActivity extends AppCompatActivity {
         int image = intent.getExtras().getInt("thumbnail_detail");
 
         imagem_post_rede_social.setImageResource(image);
+
+        btn_compartilhar = (Button) findViewById(R.id.btn_compartilhar_id);
+        btn_compartilhar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Context context = getApplicationContext();
+                CharSequence text = "Hello toast!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
 
 
     }
