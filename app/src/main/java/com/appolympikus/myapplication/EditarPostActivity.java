2511571b1d6af529032d37cb3c33300bd66b3a63
Unknown <arthur.gomes_4718@hotmail.com>
2018-03-27 09:24:04 -0300

    Share Dialog
    Incrementada a acäo de incrementar

--- app/src/main/java/com/appolympikus/myapplication/EditarPostActivity.java
+++ app/src/main/java/com/appolympikus/myapplication/EditarPostActivity.java
@@ -8,12 +8,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.content.Context;
 import android.net.Uri;
 import android.widget.Toast;import android.content.Context;
 import android.widget.Toast;
+
+import com.facebook.CallbackManager;
 import com.facebook.share.model.SharePhoto;
 import com.facebook.share.model.ShareLinkContent;
 import com.facebook.share.model.SharePhotoContent;
@@ -27,12 +30,17 @@ public class EditarPostActivity extends AppCompatActivity {
 
     private ImageView imagem_post_rede_social;
     private Button btn_compartilhar;
+    private CallbackManager callbackManager;
+    private ShareDialog shareDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editar_post);
         FacebookSdk.sdkInitialize(getApplicationContext());
+        //init fb
+        callbackManager = CallbackManager.Factory.create();
+        shareDialog = new ShareDialog(this);
 
         //getActionBar().hide();
         getSupportActionBar().hide();
@@ -44,30 +52,22 @@ public class EditarPostActivity extends AppCompatActivity {
         imagem_post_rede_social.setImageResource(image);
 
         btn_compartilhar = (Button) findViewById(R.id.btn_compartilhar_id);
+
         btn_compartilhar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ShareLinkContent content = new ShareLinkContent.Builder()
-                        .setContentUrl(Uri.parse("https://developers.facebook.com"))
+
+                ShareLinkContent linkContent = new ShareLinkContent.Builder()
+                        .setQuote("This a useful link ")
+                        .setContentUrl("https://youtube.com")
                         .build();
 
-                /*
-                Bitmap image_fb = BitmapFactory.decodeResource(getResources(), R.id.img_thumbnail_editar_id);
-                SharePhoto photo = new SharePhoto.Builder()
-                        .setBitmap(image_fb)
-                        .build();
-                SharePhotoContent content = new SharePhotoContent.Builder()
-                        .addPhoto(photo)
-                        .build();
+                if(ShareDialog.canShow(ShareLinkContent.Cla))
 
-                Context context = getApplicationContext();
-                CharSequence text = "Hello toast!";
-                int duration = Toast.LENGTH_SHORT;
 
-                Toast toast = Toast.makeText(context, text, duration);
-                toast.show(); */
 
-                //PostFB();
+
+
 
             }
         });
