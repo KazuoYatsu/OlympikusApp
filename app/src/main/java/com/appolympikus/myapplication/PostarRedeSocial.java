235c4a2e49d6c8dfef6452104c987af08739d0f7
Unknown <arthur.gomes_4718@hotmail.com>
2018-04-06 09:48:17 -0300

    Dialogo postagem rede social

---
+++ app/src/main/java/com/appolympikus/myapplication/PostarRedeSocial.java
@@ -0,0 +1,22 @@
+package com.appolympikus.myapplication;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class PostarRedeSocial extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_postar_rede_social);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        
+    }
+
+}
