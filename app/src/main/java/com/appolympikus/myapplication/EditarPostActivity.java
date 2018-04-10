9489c074c210d6692f0d71d5e2e918095f8a5cd5
Unknown <arthur.gomes_4718@hotmail.com>
2018-04-10 16:10:32 -0300

    Custom Toolbar - Detalhes no Postar Rede Social

--- app/src/main/java/com/appolympikus/myapplication/EditarPostActivity.java
+++ app/src/main/java/com/appolympikus/myapplication/EditarPostActivity.java
@@ -44,7 +44,8 @@ public class EditarPostActivity extends AppCompatActivity implements DialogoValo
 
     private ImageView imagem_post_rede_social;
     private EditText txt_local, txt_link;
-    private Button btn_compartilhar_foto, btn_voltar;
+    private Button btn_compartilhar_foto;
+    private ImageButton btn_voltar;
     private ImageButton btn_add_link, btn_add_local, btn_add_logo, btn_add_valor;
     private CallbackManager callbackManager;
     private ShareDialog shareDialog;
@@ -90,11 +91,11 @@ public class EditarPostActivity extends AppCompatActivity implements DialogoValo
 
         //Toolbar da pagina
         btn_compartilhar_foto = (Button) findViewById(R.id.btn_compartilhar_foto_id);
-        btn_voltar = (Button) findViewById(R.id.btn_toolbar_voltar_id);
+        btn_voltar = (ImageButton) findViewById(R.id.btn_toolbar_voltar_id);
         //Botoes da pagina.
         btn_add_local = (ImageButton) findViewById(R.id.btn_add_local_id);
         btn_add_logo = (ImageButton) findViewById(R.id.btn_editar_logo_id);
-        btn_add_valor = (ImageButton) findViewById(R.id.btn_editar_valor_id);
+        btn_add_valor = (ImageButton) findViewById(R.id.ibtn_add_valor_id);
         btn_add_link = (ImageButton) findViewById(R.id.btn_add_link_id);
 
         //Campos de texto da pagina.
